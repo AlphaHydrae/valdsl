633012d0ce5528c5743cb5b653226982a2016b6d
Simon Oulevay <simon.oulevay@gmail.com>
2017-03-04 17:19:50 +0100

    Custom haveErrors chai assertion

---
+++ spec/helper.js
@@ -0,0 +1,46 @@
+const _ = require('lodash');
+const chai = require('chai');
+
+chai.use(function(_chai, utils) {
+  utils.addChainableMethod(chai.Assertion.prototype, 'haveErrors', function(...expectedErrors) {
+
+    const obj = utils.flag(this, 'object');
+
+    const actualErrors = obj.errors;
+    new chai.Assertion(actualErrors).to.be.an('array');
+
+    _.each(actualErrors, error => {
+      error.location = error.location.toString();
+    });
+
+    expectedErrors = _.flatten(expectedErrors);
+    const missingErrors = expectedErrors.slice();
+    const extraErrors = actualErrors.slice();
+
+    _.each(expectedErrors, expectedError => {
+      const matchingError = _.find(actualErrors, error => _.isEqual(error, expectedError));
+      if (matchingError) {
+        extraErrors.splice(extraErrors.indexOf(matchingError), 1);
+        missingErrors.splice(missingErrors.indexOf(matchingError), 1);
+      }
+    });
+
+    var messages = [];
+    if (missingErrors.length) {
+      messages.push(`The following errors were not found:\n${describeErrors(missingErrors)}`);
+    }
+    if (extraErrors.length) {
+      messages.push(`The following extra errors were found:\n${describeErrors(extraErrors)}`);
+    }
+
+    new chai.Assertion(obj.errors).assert(
+      _.isEmpty(messages),
+      `Expected specific errors.\n\n${messages.join('\n\n')}`,
+      `Expected different errors.\n\n${messages.join('\n\n')}`
+    );
+  });
+});
+
+function describeErrors(errors) {
+  return errors.map(error => JSON.stringify(error)).join('\n');
+}
